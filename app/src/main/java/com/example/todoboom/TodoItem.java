b96805d27a5aee5dd27685ad208b341230248713
amit-schwartz-u <57283327+amit-schwartz-u@users.noreply.github.com>
2020-04-09 15:30:32 +0300

    add fields to Todo Item

--- app/src/main/java/com/example/todoboom/TodoItem.java
+++ app/src/main/java/com/example/todoboom/TodoItem.java
@@ -1,12 +1,20 @@
 package com.example.todoboom;
 
+import java.util.Date;
+
 public class TodoItem {
     String description;
     Boolean isDone;
+    Date creationTimestamp;
+    Date editTimestamp;
+    int id;
 
-    public TodoItem(String description, Boolean isDone) {
+    public TodoItem(String description, Boolean isDone, Date creationTimestamp, Date editTimestamp, int id) {
         this.description = description;
         this.isDone = isDone;
+        this.creationTimestamp = creationTimestamp;
+        this.editTimestamp = editTimestamp;
+        this.id = id;
     }
 
     public String getDescription() {
@@ -24,4 +32,29 @@ public class TodoItem {
     public void setIsDone(Boolean isDone) {
         this.isDone = isDone;
     }
+
+    public Date getCreationTimestamp() {
+        return creationTimestamp;
+    }
+
+    public void setCreationTimestamp(Date creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+    }
+
+    public Date getEditTimestamp() {
+        return editTimestamp;
+    }
+
+    public void setEditTimestamp(Date editTimestamp) {
+        this.editTimestamp = editTimestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
